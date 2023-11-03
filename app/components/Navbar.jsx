97991c55e0414fe00837006e69753409e9249b3a
Dishank Patil <dishankjp05@gmail.com>
2023-11-03 23:52:35 +0530

    add navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,19 +1,51 @@
 import React from "react";
 
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const d = new Date();
+const year = d.getFullYear();
+const date = d.getDate();
+const month = months[d.getMonth()];
+const day = weekday[d.getDay()];
 export default function Navbar() {
   return (
-    <nav>
-      <h1>TaskTracker</h1>
-      <div className='bg-slate-200 flex gap-4 items-center w-fit px-2 rounded-full m-2'>
-        <a className='hover:border-2 border-red-900 rounded-2xl px-2' href='/'>
+    <nav className='flex items-center mx-10 bg-[#F3F3F3] rounded-full pl-5 pr-24 w-fit max-container'>
+      <h1 className='text-[#013FCB] font-bold max-sm:text-6xl text-4xl m-5'>
+        Task<span className='text-black'>Tracker</span>
+      </h1>
+      <div className='flex justify-start gap-16 max-sm:text-2xl text-lg px-32 items-center font-semibold m-2'>
+        <a href='/' className='btn-hover'>
           Dashboard
         </a>
-        <a
-          className='hover:border-2 border-red-900 rounded-2xl px-2'
-          href='/tickets'
-        >
+        <a href='/tickets' className='btn-hover'>
           Tickets
         </a>
+      </div>
+      <div className='max-sm:hidden '>
+        <span className='whitespace-nowrap font-semibold text-gray-600'>
+          {day}, {date} {month} {year}
+        </span>
       </div>
     </nav>
   );
