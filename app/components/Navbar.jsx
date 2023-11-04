bcc5aabc60c2f8e6dd568c1b9a6ba80785adf64a
Dishank Patil <dishankjp05@gmail.com>
2023-11-04 17:26:41 +0530

    add initial hero

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import dashboard from "./dashboard.svg";
+import ticket from "./list.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const weekday = [
   "Sunday",
@@ -31,20 +35,23 @@ const day = weekday[d.getDay()];
 export default function Navbar() {
   return (
     <nav className='flex bg-slate-200 rounded-full justify-evenly max-sm:justify-between items-center w-full max-container py-5 px-4'>
-      <h1 className='text-[#013FCB] font-bold max-sm:text-xl text-4xl '>
+      <h1 className='text-[#013FCB] font-bold max-sm:text-lg text-4xl '>
         Task<span className='text-black'>Tracker</span>
       </h1>
-      <div className='flex gap-2 sm:gap-5 sm:text-lg '>
-        <a href='/' className='btn-hover'>
-          Dashboard
-        </a>
-        <a href='/tickets' className='btn-hover'>
-          Tickets
-        </a>
+      <div className='flex gap-2 sm:gap-5 max-sm:text-sm '>
+        <Link href='/' className=' flex items-center'>
+          <Image src={dashboard} alt='dashboard-icon' width={30} />
+          <span className='btn-hover'>Dashboard</span>
+        </Link>
+        <Link href='/tickets' className=' flex items-center'>
+          <Image src={ticket} alt='ticket-icon' width={30} />
+          <span className='btn-hover'>Tickets</span>
+        </Link>
       </div>
       <div className='max-sm:hidden '>
         <span className='whitespace-nowrap font-semibold text-gray-600'>
-          {day}, {date} {month} {year}
+          {day}, <br />
+          {date} {month} {year}
         </span>
       </div>
     </nav>
--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import dashimg from "./components/dashimg.gif";
 export default function Home() {
   return (
-    <main>
-      <h1 className='font-bold'>Hello Next.js</h1>
+    <main className='max-container flex max-xl:flex-col w-full mx-16 px-5 sm:p-16'>
+      <div className='px-5 flex justify-center items-center relative bottom-32 max-sm:bottom-16 '>
+        <Image src={dashimg} />
+      </div>
+      <div className=' basis-1/2 flex flex-col gap-5 leading-7 max-sm:relative max-sm:bottom-16'>
+        <h1 className='font-bold text-2xl text-[#013FCB] '>Dashboard</h1>
+        <p className='text-gray-700 text-xl max-sm:text-sm'>
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. It has survived not only five centuries,
+          but also the leap into electronic typesetting, remaining essentially
+          unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more recently
+          with desktop publishing software like Aldus PageMaker including
+          versions of Lorem Ipsum.
+        </p>
+      </div>
     </main>
   );
 }
