c5332a01b7093d0a8ab59b65c957cc5ad425cb5f
Dishank Patil <dishankjp05@gmail.com>
2023-12-22 13:38:05 +0530

    add styles to details feature

--- app/tickets/[id]/page.jsx
+++ app/tickets/[id]/page.jsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import Image from "next/image";
 import React from "react";
 export const dynamicParams = true;
 //Static rendering : readying pages at the initial render making it faster
@@ -33,17 +34,31 @@ async function getDetails(id) {
 export default async function TicketDetails({ params }) {
   const ticket = await getDetails(params.id);
   return (
-    <main className='flex flex-col max-w-screen-xl  xl:mx-auto mx-8 gap-5 my-10 max-sm:my-2'>
+    <main className='flex flex-col max-w-screen-xl  xl:mx-auto mx-8 gap-64 my-10 max-sm:my-2 relative'>
       <nav>
         <h1 className='font-bold text-2xl text-[#013FCB] '>Ticket Details</h1>
       </nav>
+      <div className='bg-white absolute top-20 w-full rounded-t-2xl h-[32vh] z-[-2]'>
+        <Image
+          src={ticket.imgSrc}
+          fill
+          objectFit='cover'
+          className='rounded-t-2xl z-[-1]'
+        />
+        <div className='bg-gray-100 max-sm:text-xs font-bold text-slate-700 w-fit rounded-full p-1 px-2 absolute top-4 left-4'>
+          Work Assigned to :
+        </div>
+        <div className='bg-gray-100 max-sm:text-xs font-bold text-slate-700 w-fit rounded-full p-1 px-2 absolute max-sm:top-12 max-sm:left-4 top-4 right-4'>
+          Due Date : {ticket.due_date}
+        </div>
+      </div>
       <div className='bg-white max-w-screen-xl px-5 pt-5 rounded-2xl shadow-lg break-words relative '>
         <div className='flex flex-col mb-8'>
           <h3 className='text-lg font-bold text-slate-700 rounded-full bg-gray-100 p-1 px-2 '>
             {ticket.title}
           </h3>
           <small className='text-gray-500 font-semibold ml-2'>
-            Created by
+            Admin : 
             <span className='text-green-600'> {ticket.user_email}</span>
           </small>
         </div>
